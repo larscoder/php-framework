8951b9cbd5e9914934dc67b9cf37c6f132319b62
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-04-29 08:22:52 -0500

    Agregamos validación en caso de no pasar bien la vista

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -50,6 +50,15 @@ class Request
             $method
         ]);
 
-        $response->send();
+        try {
+            if ($response instanceof Response) {
+                $response->send();
+            } else {
+                throw new \Exception("Error Processing Request");
+            }
+        } catch (\Exception $e) {
+            echo "Details {$e->getMessage()}";
+        }
+
     }
 }
